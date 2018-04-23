43b24a4e9494caf47a928dbc62dbfd42ee9e2b9c
michaelzon <michael.zonneveld@live.nl>
2018-04-23 14:20:26 +0200

    update

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,29 +1,29 @@
 import csv
 import json
-
-# open the file
-open_lego_file = open('11302984_legofile_csv.csv')
-
-# read the file and seperate by comma's
-csv_file = csv.reader(open_lego_file, delimiter = ',')
-
-lego_dict = {}
-
-# skip over the first line of the file for the headers
-fields = csv_file.next()
-
-# Iterate over each row of the csv file, zip together field -> value
-for row in csv_file:
-    lego_dict.append(dict(zip(fields, row)))
-
- # Close the CSV file
- opened_file.close()
+#
+# # open the file
+# open_lego_file = open('11302984_legofile_csv.csv')
+#
+# # read the file and seperate by comma's
+# csv_file = csv.reader(open_lego_file, delimiter = ',')
+#
+# lego_dict = {}
+#
+# # skip over the first line of the file for the headers
+# fields = csv_file.next()
+#
+# # Iterate over each row of the csv file, zip together field -> value
+# for row in csv_file:
+#     lego_dict.append(dict(zip(fields, row)))
+#
+#  # Close the CSV file
+#  opened_file.close()
 
 
 
 fieldnames = ("year","num_parts_MEAN")
 
-with open('11302984_legofile_csv.csv') as csv_f:
+with open('11302984_legofile.csv') as csv_f:
     reader = csv.DictReader(csv_f, fieldnames)
     rows = list(reader)
 
