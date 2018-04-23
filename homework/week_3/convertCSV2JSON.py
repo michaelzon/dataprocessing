dc0e53c0039d2a3efd45b34812ee839d9dc3aa95
michaelzon <michael.zonneveld@live.nl>
2018-04-23 11:02:49 +0200

    update

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,10 +1,13 @@
 import csv
 import json
 
+# read csvfile and write to jsonfile
 csvlego = open('11302984_legofile_csv.csv', 'r')
 jsonlego = open('11302984_legofile_json.json', 'w')
 
 fieldnames = ("year","num_parts_MEAN")
+
+# seperate by newline and convert an object to a string with .dump
 reader = csv.DictReader(csvlego, fieldnames)
 for row in reader:
     json.dump(row, jsonlego)
