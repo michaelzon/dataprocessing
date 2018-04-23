afcc56966fd0d163d2c80d7c8c2e1f6a53de3420
michaelzon <michael.zonneveld@live.nl>
2018-04-23 13:19:42 +0200

    update

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,31 +1,29 @@
-# import csv
-# import json
-# import glob
-# import os
-#
-#     with open('11302984_legofile_csv.csv') as f:
-#         reader = csv.DictReader(f)
-#         rows = list(reader)
-#
-#     with open('11302984_legofile_json.json', 'w') as f:
-#         json.dump(rows, f)
-
-
 import csv
 import json
 
-# read csvfile and write to jsonfile
-csvlego = open('11302984_legofile_csv.csv', 'r')
-jsonlego = open('11302984_legofile_json.json', 'w')
+with open('11302984_legofile_csv.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
 
-# jsonlego_list = []
-fieldnames = ("year","num_parts_MEAN")
+with open('11302984_legofile_json.json', 'w') as f:
+    json.dump(rows, f)
 
-# seperate by newline and convert an object to a string with .dump
-reader = csv.DictReader(csvlego, fieldnames)
-for row in reader:
-    json.dump(row, jsonlego)
-    jsonlego.write
+
+# import csv
+# import json
+#
+# # read csvfile and write to jsonfile
+# csvlego = open('11302984_legofile_csv.csv', 'r')
+# jsonlego = open('11302984_legofile_json.json', 'w')
+#
+# # jsonlego_list = []
+# fieldnames = ("year","num_parts_MEAN")
+#
+# # seperate by newline and convert an object to a string with .dump
+# reader = csv.DictReader(csvlego, fieldnames)
+# for row in reader:
+#     json.dump(row, jsonlego)
+#     jsonlego.write
 
 # jsonlego_list.append(jsonlego)
 # print(jsonlego_list)
