1340cd0f55095e51188fd1e564450b8a8d5b107a
michaelzon <michael.zonneveld@live.nl>
2018-04-24 11:22:08 +0200

    update

--- homework/week_3/javascript/barchartscript.js
+++ homework/week_3/javascript/barchartscript.js
@@ -1,18 +1,20 @@
-d3.select("body")
-  .append("p")
-  .text("New paragraph!");
+// d3.select("body")
+//   .append("p")
+//   .text("New paragraph!");
 
-var lego = [ 5, 10, 15, 20, 25 ];
+var lego = [5, 10, 15, 20, 25];
+console.log(lego)
 
 // select chart id from html
 d3.select(".legochart")
   // select al divs, divs are empty tho
-  .selectAll("div");
+  .selectAll("div")
   // pass in the data
   .data(lego)
     // looks at the dom, sees there is no data, so creates 5 datapoints
     .enter()
     // appends a div at the end of datapoints
-    .append("div")
+    // .append("div")
+    // d represents every element in the array
     .style("width", function(d) { return d + "px"; })
     .text(function(d) { return d; });
