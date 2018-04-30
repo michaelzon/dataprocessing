c34b20b2c0dbca57d07ab425b414f6e6438c420f
michaelzon <michael.zonneveld@live.nl>
2018-04-30 21:46:24 +0200

    upd

---
+++ homework/week_4/scatter.js
@@ -0,0 +1,8 @@
+// Name: Michael Zonneveld
+// Studentnumber: 11302984
+
+// function that will be triggered when the page is loaded
+window.onload = function() {
+
+  console.log('Yes, you can!');
+};
