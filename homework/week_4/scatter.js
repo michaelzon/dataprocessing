ee8b944924b15fee5979648a0ddda2548b22bdd0
michaelzon <michael.zonneveld@live.nl>
2018-05-03 10:57:20 +0200

    upd

--- homework/week_4/scatter.js
+++ homework/week_4/scatter.js
@@ -1,34 +1,61 @@
 // Name: Michael Zonneveld
 // Studentnumber: 11302984
 
+
 // function that will be triggered when the page is loaded
 window.onload = function() {
+  var svg = d3.select("svg");
+  paragraph = d3.select("body")
+  paragraph.append("p").text("Name: Michael Zonneveld")
+  paragraph.append("p").text("Studentnummer: 11302984")
+  paragraph.append("p").text("Insert information here")
 
   // retrieve data from api and store the queries
-  var employmentStrictness = "http://stats.oecd.org/SDMX-JSON/data/EPL_R/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.EPR_V3";
-  var employmentRate =   "http://stats.oecd.org/SDMX-JSON/data/STLABOUR/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.LREM64TT.STSA.A/all?startTime=2008&endTime=2013";
-  var gdpPpp = "http://stats.oecd.org/SDMX-JSON/data/PDB_LV/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA.T_GDPPOP.VPVOB/all?startTime=2008&endTime=2013";
+  wellBeing = "http://stats.oecd.org/SDMX-JSON/data/RWB/AUS+AUT+BEL+CAN+CZE+DNK+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ITA+JPN+KOR+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+ESP+SWE+CHE+TUR+GBR+USA.VOTERS_SH+BB_ACC+SUBJ_PERC_CORR.VALUE/all?startTime=2014&endTime=2014"
 
   // request for the queries
   d3.queue()
-  .defer(d3.request, employmentStrictness)
-  .defer(d3.request, employmentRate)
-  .defer(d3.request, gdpPpp)
+  .defer(d3.request, wellBeing)
   .awaitAll(getData);            // due to asynchronounity wait for all
 
-  function getData(error, response) {
-    if (error) throw error;
+  makeCanvas();
 
-    // parse gigantic string to nice json format
-    var data0 = JSON.parse(response[0].responseText);
-    console.log("employmentStrictness: ",data0.dataSets)
+};
 
-    // parse gigantic string to nice json format
-    var data1 = JSON.parse(response[1].responseText);
-    // console.log("employmentRate: ",data1.dataSets)
+function getData(error, response) {
+  if (error) throw error;
 
-    // parse gigantic string to nice json format
-    var data2 = JSON.parse(response[2].responseText);
-    // console.log("gdpPpp:",data2.dataSets)
-  };
+  countryArray = []
+  for(var i = 0; i < 27; i++){
+    data = JSON.parse(response[0].responseText)
+    countryArray.push(data.structure.dimensions.series[0].values[i]["name"])
+  }
+  console.log(countryArray)
+
+  // internetArray = []
+  //
+  //   data = JSON.parse(response[0].responseText)
+  //   axes = data.dataSets[0] //nog iets
+  //   for(var i = 0; i < 27; i++){
+  //     for (var j = 0; i < 2; j++){
+  //       connect = i + ":" + j + ":0:0"
+        // console.log(axes)
+//       }
+//   }
+//
+};
+
+
+
+function makeCanvas(){
+width = 1200;
+height = 800;
+padding = 200;
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+
 };
