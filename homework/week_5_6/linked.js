2f97f3685549232ac3a079476ef10bfecab6835e
michaelzon <michael.zonneveld@live.nl>
2018-05-09 13:19:16 +0200

    upd

--- homework/week_5_6/linked.js
+++ homework/week_5_6/linked.js
@@ -1,3 +1,9 @@
+
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+
+var width = 720 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
 // function that will be triggered when the page is loaded
 window.onload = function() {
 
@@ -11,6 +17,9 @@ window.onload = function() {
   .defer(d3.request, income)
   .awaitAll(getData);
 
+  createWebInfo();
+  createMap();
+  createChart();
 };
 
 function getData(error, response) {
@@ -182,43 +191,56 @@ function getData(error, response) {
   console.log(incomeDict)
 };
 
-function makeMap(){
-  var width = 960,
-    height = 550;
+function createWebInfo(){
 
-var colour = d3.scale.category20();
-
-var projection = d3.geo.mercator()
-    .scale(1)
-    .translate([0, 0]);
-
-var path = d3.geo.path()
-    .projection(projection);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-d3.json("nld.json", function(error, nld) {
-
-    var l = topojson.feature(nld, nld.objects.subunits).features[3],
-        b = path.bounds(l),
-        s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-
-    projection
-        .scale(s)
-        .translate(t);
-
-    svg.selectAll("path")
-        .data(topojson.feature(nld, nld.objects.subunits).features).enter()
-        .append("path")
-        .attr("d", path)
-        .attr("fill", function(d, i) {
-            return colour(i);
-        })
-        .attr("class", function(d, i) {
-            return d.properties.name;
-        });
-});
+  // writing information to webpage per paragraph
+  paragraph = d3.select("header")
+  paragraph.append("p").text("Name: Michael Zonneveld")
+  paragraph.append("p").text("Studentnummer: 11302984")
+  paragraph.append("p").text("This webpage shows 2 interactive visualizations ")
 }
+
+function createMap(){
+
+  var colour = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var projection = d3.geoMercator()
+      .scale(1)
+      .translate([0, 0]);
+
+  var path = d3.geoPath()
+      .projection(projection);
+
+  var svg = d3.select("#map").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  d3.json("nld.json", function(error, nld) {
+
+      var l = topojson.feature(nld, nld.objects.subunits).features[3],
+          b = path.bounds(l),
+          s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+          t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+      projection
+          .scale(s)
+          .translate(t);
+
+      svg.selectAll("path")
+          .data(topojson.feature(nld, nld.objects.subunits).features).enter()
+          .append("path")
+          .attr("d", path)
+          .attr("fill", function(d, i) {
+              return colour(i);
+          })
+          .attr("class", function(d, i) {
+              return d.properties.name;
+          });
+    });
+}
+
+function createChart(){
+
+
+
+};
